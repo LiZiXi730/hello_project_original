5303a927e223ce95ce406b14ed6b3550edec10dd
easonl <2640249980@qq.com>
2025-07-24 13:50:44 +0800

    Initial commit

---
+++ hello_app/views.py
@@ -0,0 +1,14 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def greet(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            name = data.get('name', 'Guest')
+            return JsonResponse({'greeting': f'Hello, {name}!'})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    return JsonResponse({'error': 'Method not allowed'}, status=405)
